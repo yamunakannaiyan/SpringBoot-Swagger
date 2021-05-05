47bf28c7c539cc6524428410e0a065a3ab1a1c64
yamunakannaiyan <83700974+yamunakannaiyan@users.noreply.github.com>
2021-05-05 15:02:34 -0700

    Add files via upload

---
+++ spring-swagger/src/main/java/com/coderulagam/SpringSwaggerApplication.java
@@ -0,0 +1,19 @@
+package com.coderulagam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class SpringSwaggerApplication {
+	
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwaggerApplication.class, args);
+	}
+
+}
